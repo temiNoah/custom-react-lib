2e10bcfae14540208b7d0f02000c95206d5f96f9
temitope.ogundiniyi <temitope.ogundiniyi@interswitchgroup.com>
2020-07-14 13:45:51 +0100

    hello world

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ import Content from "./Content";
 
 const App = () => {
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <Header />
-      </Grid>
-      <Grid item container>
-        <Grid item xs={false} sm={2} />
-        <Grid item xs={12} sm={8}>
-          <Content />
-        </Grid>
-        <Grid item xs={false} sm={2} />
-      </Grid>
-    </Grid>
+    <div className="App">
+    <Typography variant="h1" color="primary">
+      hello world
+    </Typography>    
+   
+  </div>
   );
 };
 
