43be8cc61cfecb142468be04c4bee7f13e72580c
temitope.ogundiniyi <temitope.ogundiniyi@interswitchgroup.com>
2020-07-29 08:31:25 +0100

    renamed Form file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import Header from "./Header";
-import Content from "./Content";
+//import Header from "./Header";
+//import Content from "./Content";
+import FormUserDetails from "./components"
 
 const App = () => {
   return (
     <div className="App">
-          <FormUserDetails />
+          <FormUserDetails name="sdsS"/>
   </div>
   );
 };
