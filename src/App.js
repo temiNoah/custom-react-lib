42cf674f9bbab0966f9e65b890966f26223d56ac
temitope.ogundiniyi <temitope.ogundiniyi@interswitchgroup.com>
2020-07-29 18:41:55 +0100

    updated the form

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Grid } from "@material-ui/core";
 //import Header from "./Header";
 //import Content from "./Content";
 import FormUserDetails from "./components/FormUserDetails";
+import MenuAppBar from './components/MenuApp';
 
 const App = () => {
   return (
-     <div >
+     <div className="App" >
+           <MenuAppBar/>
            <FormUserDetails /> 
    </div>
   );
--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -4,8 +4,33 @@ import AppBar from '@material-ui/core/AppBar';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
  class FormUserDetails extends Component {
+  
+   
+
     constructor(props) {
         super(props);
         this.state ={
@@ -39,6 +64,8 @@ import Button from '@material-ui/core/Button';
 
   render() {    // return (<h1>hevv</h1>);
    // const { values, handleChange } = this.props;
+
+  //const classes = useStyles();
     return (
       <MuiThemeProvider>
         <>
@@ -47,39 +74,44 @@ import Button from '@material-ui/core/Button';
             fullWidth
             maxWidth='sm'
           > */}
-            <AppBar title="Enter User Details" />
-            <TextField
-              placeholder="Enter Your First Name"
-              label="First Name"
-              onChange={this.handleChange('firstName')}
-              defaultValue={this.firstName}
-              margin="normal"
-              fullWidth
-            />
-            <br />
-            <TextField
-              placeholder="Enter Your Last Name"
-              label="Last Name"
-              onChange={this.handleChange('lastName')}
-              defaultValue={this.lastName}
-              margin="normal"
-              fullWidth
-            />
-            <br />
-            <TextField
-              placeholder="Enter Your Email"
-              label="Email"
-              onChange={this.handleChange('email')}
-              defaultValue={this.email}
-              margin="normal"
-              fullWidth
-            />
-            <br />
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={this.continue}
-            >Continue</Button>
+            <Container maxWidth="sm">
+            {/* <Card className={classes.root}>
+              <CardContent> */}
+                    <TextField
+                      placeholder="Enter Your First Name"
+                      label="First Name"
+                      onChange={this.handleChange('firstName')}
+                      defaultValue={this.firstName}
+                      margin="normal"
+                      fullWidth
+                    />
+                    <br />
+                    <TextField
+                      placeholder="Enter Your Last Name"
+                      label="Last Name"
+                      onChange={this.handleChange('lastName')}
+                      defaultValue={this.lastName}
+                      margin="normal"
+                      fullWidth
+                    />
+                    <br />
+                    <TextField
+                      placeholder="Enter Your Email"
+                      label="Email"
+                      onChange={this.handleChange('email')}
+                      defaultValue={this.email}
+                      margin="normal"
+                      fullWidth
+                    />
+                    <br />
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      onClick={this.continue}
+                    >Continue</Button>
+                    </Container>
+              {/* </CardContent>
+            </Card> */}
           {/* </Dialog> */}
         </>
       </MuiThemeProvider>
