4596a6d478a85a5861c841eb36da399849b93a93
temitope.ogundiniyi <temitope.ogundiniyi@interswitchgroup.com>
2020-07-30 11:27:56 +0100

    added form validation

--- src/util/formUtil.js
+++ src/util/formUtil.js
@@ -11,7 +11,34 @@ export function isValidEmail(email) {
         /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
       );
     
-      return validEmailRegex.test(email);
+      return validEmailRegex.test(email) ? "" : "is not a valid email";
+}
+
+
+export const isValidPhoneNumber = (phone) =>{
+
+    if (phone !== "undefined") {        
+
+        var pattern = new RegExp(/^[0-9\b]+$/);
+      
+        if (!pattern.test(phone)) {
+      
+         // isValid = false;
+      
+        //  errors["phone"] =
+          return  "Please enter only number.";
+      
+        }else if(phone.length !== 10){
+      
+         // isValid = false;
+      
+          return  "Please enter valid phone number.";
+      
+        }
+
+        return '';
+      
+      }
 }
 
 export function isValidUsername(username){  
@@ -20,21 +47,25 @@ export function isValidUsername(username){
             return 'This field cannot be empty.'
         }
 
+        return "";
+
 }
 
 export function isValidPassword(password){
  
     // null check
         if (!password) {
-            password= 'This field cannot be empty.'
+          return  password= 'This field cannot be empty.'
         }
         // After null checking, check length
-        else if (values.password1.length < 8) {
-            password = 'The password provided is not long enough.'
+        else if (password.length < 8) {
+           return password = 'The password provided is not long enough.'
         }
         //To check a password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
         else if ( /^[A-Za-z]\w{7,14}$/g.test(password))   //(/([A-Z]+)/g.test(values.password1)) 
         {
-            password = 'You don\'t have any capital letters in there yo'
+          return   password = 'You don\'t have any capital letters in there yo'
         } 
+
+        return "";
  }
